e8b6189750a0c259563dfad546a3c03df7332117
Pavan5230 <104582622+Pavan5230@users.noreply.github.com>
2023-07-19 15:57:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+function bulboff(){
+    document.getElementById("bulb").src = "https://th.bing.com/th/id/OIP.ZNlnbbyJ0QapDcig1E7ngwAAAA?w=122&h=185&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+}
+
+function bulbon(){
+    document.getElementById("bulb").src="https://th.bing.com/th/id/OIP.-SBftY8cFPKO9XKYOx_7AwHaHa?w=170&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+}
